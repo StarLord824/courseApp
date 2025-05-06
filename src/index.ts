a113a15620293ceb0b8550977108cea5469db2dc
abhi <shuklaabhinav824@gmail.com>
2025-05-06 18:51:34 +0530

    user route skeleton

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import userRouter from "./routes/userRouter";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/user', userRouter);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the 100xDeveloper API!");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
---
+++ src/routes/userRouter.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+const client = new PrismaClient();
+
+router.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  await client.user.create({
+    data: {
+      name,
+      email,
+      password,
+    },
+  });
+
+  res.json({ message: "User registered successfully" });
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  await client.user
+    .findUnique({
+      where: {
+        email,
+        password
+      },
+    })
+    .then((user) => {
+      if (user) {
+        const token = jwt.sign({ userId: user.id }, "secret");
+        res.json({ token });
+      } else {
+        res.status(401).json({ message: "Invalid email or password" });
+      }
+    });
+});
+
+export default router;
