71b96161cf8874d0085a67bee2e1f44fe396e818
StarLord824 <shuklaabhinav824@gmail.com>
2025-05-10 10:33:56 +0530

    undo prev commit

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,6 @@ app.get("/", (req, res) => {
     res.send("Welcome to the 100xDeveloper API!");
 });
 
-
-//frontend route
-
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
